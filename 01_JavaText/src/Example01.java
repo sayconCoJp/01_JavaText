75e9d07cb5e7b004188d91e682687ea456bd5c29
Yusei <saycon.co.jp@gmail.com>
2022-10-30 08:47:14 +0900

    てすと

--- 01_JavaText/src/Example01.java
+++ 01_JavaText/src/Example01.java
@@ -1,6 +1,7 @@
 public class Example01 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
 }
